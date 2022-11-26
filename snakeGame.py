0f2104ffe51e160b82a3c8c4d8340de73b4d753f
Robert Myers jr <myersjrr@msoe.edu>
2022-11-26 09:53:35 -0600

    Made a window appear with a white square

--- snakeGame.py
+++ snakeGame.py
@@ -1,10 +1,19 @@
 import pygame
 
 pygame.init()
-
+#size of window
 size = width, height = 600, 600
+#colors
+black = [0, 0, 0]
+white = [255, 255, 255]
 
 screen = pygame.display.set_mode(size)
 
 while True:
-    screen.fill([0,0,0])
+    screen.fill(black)
+
+    pygame.draw.rect(screen,white,pygame.Rect(10,30,60,60))
+    pygame.display.flip()
+
+
+
